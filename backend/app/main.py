c7b4fff2d636a605cba4f798f743961798dee452
Zeyad El-Sayed Yousif <83798621+zeyadusf@users.noreply.github.com>
2026-03-23 11:48:20 +0200

    update : origins urls in backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -50,7 +50,7 @@ app.add_middleware(
     allow_origins=[
     "http://localhost:5500",
     "http://127.0.0.1:5500",
-    "https://tesseractrag.pages.dev",],	
+    "https://tesseractrag.ziayd-usf.workers.dev",],	
     allow_methods=["*"],
 	allow_headers=["*"],
 )
